c683965cdd783059884dbb3a8bd05d428a5b8b7d
Kumar <harshkumar@paypalcorp.com>
2016-08-04 18:07:32 +0530

    FinalCode

--- UpdateScore.java
+++ UpdateScore.java
@@ -2,9 +2,44 @@ package wordsFreq;
 
 public class UpdateScore {
 	
-
-	static int score = 5431;
+	//score format - gameAgameBsetAsetBmatchAmatchB
+	//eg-114531 where 31 indicates 40-15 
+	static int score = 115431;
 	static boolean cont_game = true;
+	public static void parseScore(String input)
+	{
+	    char[] scorers = input.toCharArray();
+	    for(char scorer : scorers)
+	    {
+	        if(!updateScore(scorer))
+	        {
+	            displayScore(score);
+	        }
+	    }
+	    System.out.println("Final Score");
+	    displayScore(score);
+	}
+	
+	public static void displayScore(int inputScore)
+	{
+	    String[] points = {"0","15","30","40","A"};
+	    int setScoreA = inputScore/100000;
+	    inputScore=inputScore%100000;
+	    int setScoreB = inputScore/10000;
+	    inputScore = inputScore%10000;
+	    int gameScoreA = inputScore/1000;
+	    inputScore = inputScore%1000;
+	    int gameScoreB = inputScore/100;
+	    inputScore = inputScore%100;
+	    String pointsA = points[inputScore/10];
+	    inputScore = inputScore%10;
+	    String pointsB = points[inputScore];
+	    System.out.println("  Set   Game   Points");
+	    System.out.println("A  "+setScoreA+"      "+gameScoreA+"      "+pointsA);
+	    System.out.println("B  "+setScoreB+"      "+gameScoreB+"      "+pointsB);
+	    
+	    
+	}
 	public static boolean updateScore(char winner)
 	{
 		incrementScore(winner);
